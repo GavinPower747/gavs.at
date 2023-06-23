41eacb53337c34d204be6fbe3443098f87abd4a9
Gavin Power <gavinpower747@hotmail.com>
2023-06-23 17:25:57 +0100

    Added request metrics for application insights

--- pkg/middleware/request_metrics.go
+++ pkg/middleware/request_metrics.go
@@ -1,13 +1,38 @@
 package middleware
 
 import (
-	"log"
 	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/microsoft/ApplicationInsights-Go/appinsights"
+
+	"gavs.at/shortener/pkg/web"
 )
 
 func RequestMetrics(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Println("RequestMetricsMiddleware")
-		next.ServeHTTP(w, r)
+		startTime := time.Now()
+
+		recorder := &web.StatusRecorder{
+			ResponseWriter: w,
+			StatusCode:     http.StatusOK,
+		}
+
+		next.ServeHTTP(recorder, r)
+
+		duration := time.Since(startTime)
+
+		connectionString := os.Getenv("APPINSIGHTS_CONNECTION_STRING")
+
+		if connectionString == "" {
+			return
+		}
+
+		client := appinsights.NewTelemetryClient(connectionString)
+		trace := appinsights.NewRequestTelemetry(r.Method, r.URL.String(), duration, strconv.Itoa(recorder.StatusCode))
+
+		client.Track(trace)
 	})
 }
---
+++ pkg/web/status_recorder.go
@@ -0,0 +1,15 @@
+package web
+
+import "net/http"
+
+// Wrapper for http.ResponseWriter that keeps track of the status code for logging/metrics
+// Satifies the http.ResponseWriter interface so it can be passed in its place
+type StatusRecorder struct {
+	http.ResponseWriter
+	StatusCode int
+}
+
+func (r *StatusRecorder) WriteHeader(statusCode int) {
+	r.StatusCode = statusCode
+	r.ResponseWriter.WriteHeader(statusCode)
+}
